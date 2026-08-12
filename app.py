36b2054c8b110a9a3dd663f5fdf99668c979f5c7
KETSRANA866 <kets.rana@gmail.com>
2026-08-12 16:41:43 +0530

    Change app port from 5000 to 5003

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello , app deployed using GitHub Actions and Docker Host!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5003)
